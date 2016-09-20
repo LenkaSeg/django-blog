66fdd887476eea655cce6b8f34560693acb7e508
Lenka Segura <lenka@sepu.cz>
2016-09-20 16:01:47 +0200

    Postgresql database

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'MyDjangoBlogDatabase',
+        'USER': 'superuser',
+        'PASSWORD': 'cogombret',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
